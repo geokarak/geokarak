894922a066cbfbe0830d6370528ff54853b93291
Georgios Karakostas <karakostas.georgios@gmail.com>
2024-01-21 21:50:20 +0100

    Update bio

--- geokarak.py
+++ geokarak.py
@@ -7,17 +7,15 @@ from rich.tree import Tree
 console = Console(record=True, width=100)
 
 tree = Tree(":bust_in_silhouette: Georgios Karakostas", guide_style="bold cyan")
-data_science_tree = tree.add(":snake: [link=https://www.linkedin.com/in/geokarak]Data Scientist", guide_style="green")
-data_science_tree.add("Machine Learning")
-data_science_tree.add("Storytelling")
-data_science_tree.add("Automation")
+data_science_tree = tree.add(":snake: [link=https://www.linkedin.com/in/geokarak]MLOps Engineer", guide_style="green")
+# data_science_tree.add("Machine Learning")
+# data_science_tree.add("Storytelling")
+# data_science_tree.add("Automation")
 photographer_tree = tree.add(":camera: [link=https://www.georgios-karakostas.com]Photographer")
 mountain_bike_tree = tree.add(":bicyclist: [link=https://www.strava.com/athletes/geokarak]Mountain biker")
 
 about = """\
-I am a mathematician by training and a photographer by heart, with +3 years of professional experience in the field of Data Science.
-
-[green]Follow me on twitter [bold link=https://twitter.com/geok8s]@geok8s[/]"""
+I am a mathematician by training and a photographer by heart, working professionally in the field of AI and Machine Learning since 2019."""
 
 panel = Panel.fit(
     about, box=box.DOUBLE, border_style="blue", title="[b]Bio", width=60
